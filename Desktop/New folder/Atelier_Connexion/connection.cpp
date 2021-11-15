80d4590aaf723ad51692b4c93ca9c0f54e8e9399
Yessine68 <don7allouzi@gmail.com>
2021-11-15 20:44:52 +0000

    my first commit

---
+++ Desktop/New folder/Atelier_Connexion/connection.cpp
@@ -0,0 +1,24 @@
+#include "connection.h"
+#include<QDebug>
+
+Connection::Connection()
+{
+
+}
+
+bool Connection::createconnect()
+{bool test=false;
+QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
+db.setDatabaseName("Source_Projet2A");
+db.setUserName("jarray");//inserer nom de l'utilisateur
+db.setPassword("jarray");//inserer mot de passe de cet utilisateur
+
+if (db.open())
+test=true;
+
+
+
+
+
+    return  test;
+}
